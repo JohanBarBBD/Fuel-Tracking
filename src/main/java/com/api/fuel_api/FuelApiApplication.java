16fa7a017d25f3e3e12872d7ac18a14a1297ae83
reece-stephenson <reeces@bbd.co.za>
2023-03-17 16:11:19 +0200

    Added Project Setup Files

---
+++ src/main/java/com/api/fuel_api/FuelApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.fuel_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelApiApplication.class, args);
+	}
+
+}
